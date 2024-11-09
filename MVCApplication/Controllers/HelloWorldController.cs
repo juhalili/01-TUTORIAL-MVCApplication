edaf9ec4561e1e03f20f61a9e42d1c49dcbfabf8
Juled Halili <juhalili@vsite.hr>
2024-11-09 15:29:59 +0100

    Niti cetvrtina tutoriala

---
+++ MVCApplication/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is the index page";
+        }
+
+        public string Hello()
+        {
+            return "Hello";
+        }
+    }
+}
